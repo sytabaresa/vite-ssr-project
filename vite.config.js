1965d67d781dc8ba2539ea94c076419350ac480e
Sebastian Tabares <sytabaresa@gmail.com>
2023-04-27 13:37:57 -0500

    first version

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
+import {
+    VitePWA
+} from 'vite-plugin-pwa'
 
 export default {
-  plugins: [react(), ssr()]
+    plugins: [
+        react(),
+        ssr({
+            prerender: true
+        }),
+        VitePWA({
+            registerType: 'autoUpdate'
+        })
+    ]
 }
